ed4c5be987264bb5cfcf3bb516659a0a13a934f2
Christopher <christho987@hotmail.com>
2022-11-19 18:16:11 -0500

    styles pokedex and APIs name

--- JS/Pokedex/js/pokedex.js
+++ JS/Pokedex/js/pokedex.js
@@ -16,8 +16,16 @@ const fetchPokemon =async () => {
         console.log(data);
         let pokeImg = data.sprites.front_default;
         let pokeInfo= data.abilities;
+        let pokeNames=data.name;
+        let pokeID=data.id;
+        let pokeWeight=data.weight;
+        let pokeHeight=data.height;
         pokeImage(pokeImg);
         pokeData(pokeInfo);
+        pokemonName(pokeNames);
+        pokemonIdNames(pokeID);
+        pokemonHeight(pokeHeight);
+        pokemonWeight(pokeWeight);
         console.log(pokeImg);
     }
 }
@@ -33,6 +41,26 @@ const pokeData=(abilities)=>{
     const abilitiesName = abilities.map(item=> item.ability.name);
     //console.log('abilities full', abilities);
     //console.log('abilities Names', abilitiesName);
-    pokeAbilities.innerHTML=abilitiesName;
+    pokeAbilities.innerHTML=  abilitiesName;
 
+}
+
+const pokemonName=(names)=>{
+    const pokeNames=document.getElementById("names");
+    pokeNames.innerHTML= '<h2>'+names. toUpperCase()+ '</h2>';
+}
+
+const pokemonIdNames=(idNames)=>{
+    const pokeNames=document.getElementById("idNames");
+    pokeNames.innerHTML= '<h2> #'+idNames+'</h2>';
+}
+
+
+const pokemonWeight=(peso)=>{
+    const pokeNames=document.getElementById("peso");
+    pokeNames.innerHTML= '<h2>Peso: '+peso+' Kg</h2>';
+}
+const pokemonHeight=(altura)=>{
+    const pokeNames=document.getElementById("altura");
+    pokeNames.innerHTML= '<h2>Altura: '+altura+' m</h2>';
 }
