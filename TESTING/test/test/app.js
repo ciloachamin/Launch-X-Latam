25c2327e624e92007006e0f0fde96c33c7e1ad6d
Christopher <christho987@hotmail.com>
2022-12-12 10:04:14 -0500

    Create file test

--- TESTING/test/test/app.js
+++ TESTING/test/test/app.js
@@ -15,16 +15,19 @@ describe("Color Code Converter", function(){
         });
 
     });
-
     describe("HEX to RGB conversion",function(){
-        if("converts basic color", function(){
-            let redRgb = converter.hexToRgb("ff0000");
-            let greenRgb =converter.hexToRgb("00ff00");
-            let blueRgb =converter.hexToRgb("0000ff");
+        it("converts basic color", function(){
+            let red = converter.hexToRgb("ff0000");
+            let green =converter.hexToRgb("00ff00");
+            let blue =converter.hexToRgb("0000ff");
 
-            expect(redRgb).to.deep.equal([255,0,0]);
-            expect(greenRgb).to.deep.equal([0,255,0]);
-            expect(blueRgb).to.deep.equal([0,0,255]);
+            expect(red).to.deep.equal([255,0,0]);
+            expect(green).to.deep.equal([0,255,0]);
+            expect(blue).to.deep.equal([0,0,255]);
+            
         });
+
     });
+
+    
 });
